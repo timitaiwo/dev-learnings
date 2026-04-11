f73a0fe676f6793f09a21c2af7ce08f83eb0aa8a
Timi Taiwo <31576028+timitaiwo@users.noreply.github.com>
2026-04-11 10:22:13 +0100

    Add wrapper code for the hashmap

---
+++ swiss_table_hashmap_c++/src/swiss_table.cpp
@@ -0,0 +1,16 @@
+
+
+template<typename K, typename V>
+class hash_table {
+public:
+    V get(K key) {
+        
+    }
+
+    bool store(K key, V value) {
+        return false;
+    }
+
+private:
+
+};
