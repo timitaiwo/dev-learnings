0c32bfa39aec3d37960071eb3d4ad37be0e0bd16
Timi Taiwo <31576028+timitaiwo@users.noreply.github.com>
2026-05-10 07:56:32 +0100

    Update swiss_table.cpp API

--- swiss_table_hashmap_c++/src/swiss_table.cpp
+++ swiss_table_hashmap_c++/src/swiss_table.cpp
@@ -1,18 +1,31 @@
 template<typename K, typename V>
 class hash_table {
 public:
-    V get(K key, V default_value) {
+    V* get(K& key) {
         
     }
 
-    void delete_key(K key) {
+    void remove(K& key) {
         
     }
 
-    bool store(K key, V value) {
+    bool insert(K& key, V& value) {
         return false;
+    }
+
+    int size() {
+        
+    }
+
+    bool clear() {
+        
+    }
+
+    bool resize(int new_capacity) {
+        
     }
 
 private:
 
 };
+  
